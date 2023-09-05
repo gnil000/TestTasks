ff269479ddc68283df8224a02a19b053114ae978
Elena_Gnil <83718500+gnil000@users.noreply.github.com>
2023-09-05 17:32:08 +0300

    Add files via upload

---
+++ Figure.cs
@@ -0,0 +1,30 @@
+﻿namespace FigureLibrary
+{
+    public class Figure
+    {
+
+        public double CalculateArea(double radius)
+        {
+            return Math.PI * radius * radius;
+        }
+
+        public double CalculateArea(double a, double b, double c)
+        {
+            double p = (a + b + c) / 2;
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+
+        public bool IsRightTriangle(double a, double b, double c)
+        {
+            double[] arr = { a, b, c };
+            Array.Sort(arr);
+            double sideA = arr[2];
+            double sideB = arr[1];
+            double sideC = arr[0];
+            if ((sideA * sideA) == ((sideB * sideB) + (sideC * sideC)))
+                return true;
+            return false;
+        }
+
+    }
+}
---
+++ FigureTest.cs
@@ -0,0 +1,124 @@
+using FigureLibrary;
+
+namespace TestProject4
+{
+    [TestClass]
+    public class FigureTest
+    {
+        [TestMethod]
+        public void CalculateArea_0_0returned()
+        {
+            // arrange
+            double x = 0;
+            double expected = 0;
+            // act
+            Figure a = new Figure();
+            double actual = a.CalculateArea(x);
+            // assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void CalculateArea_1_3returned()
+        {
+            // arrange
+            double x = 1;
+            double expected = 3.141592653589793;
+            // act
+            Figure a = new Figure();
+            double actual = a.CalculateArea(x);
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CalculateArea_2_6returned()
+        {
+            // arrange
+            double x = 2;
+            double expected = 12.566370614359172;
+            // act
+            Figure a = new Figure();
+            double actual = a.CalculateArea(x);
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CalculateArea_0and0and0_0returned()
+        {
+            // arrange
+            double x = 0;
+            double y = 0;
+            double z = 0;
+            double expected = 0;
+            // act
+            Figure a = new Figure();
+            double actual = a.CalculateArea(x, y, z);
+            // assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void CalculateArea_1and1and1_0returned()
+        {
+            // arrange
+            double x = 1;
+            double y = 1;
+            double z = 1;
+            double expected = 0.4330127018922193;
+            // act
+            Figure a = new Figure();
+            double actual = a.CalculateArea(x, y, z);
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CalculateArea_3and4and5_6returned()
+        {
+            // arrange
+            double x = 3;
+            double y = 4;
+            double z = 5;
+            double expected = 6;
+            // act
+            Figure a = new Figure();
+            double actual = a.CalculateArea(x, y, z);
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void IsRightTriangle_3and4and5_1returned() {
+            // arrange
+            double x = 3;
+            double y = 4;
+            double z = 5;
+            bool expected = true;
+            // act
+            Figure a = new Figure();
+            bool actual = a.IsRightTriangle(x, y, z);
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void IsRightTriangle_1and1and1_0returned()
+        {
+            // arrange
+            double x = 1;
+            double y = 1;
+            double z = 1;
+            bool expected = false;
+            // act
+            Figure a = new Figure();
+            bool actual = a.IsRightTriangle(x, y, z);
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
